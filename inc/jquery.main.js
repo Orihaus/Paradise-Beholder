f8bb8afb5a3e4389ab8f908cf6d98cc4e9f20084
Devine Lu Linvega <aliceffekt@gmail.com>
2015-08-24 12:56:37 -0400

    Added full universe download

--- inc/jquery.main.js
+++ inc/jquery.main.js
@@ -1,48 +1,72 @@
-var owner = 1;
-var origin = 1;
+// Config
+
+var universeId = 1;
+
+// World
+
+var universeArray = [];
+
+// User
+
+var userLocation = 1;
 
 $(document).ready(function()
 {
-	apiQuery(origin);
+	loadUniverse(universeId);
+
+	console.log(universeArray)
 
 	$("vessel").live("click", function(){ 
 		targetLocation = $(this).attr("data");
-		apiQuery(targetLocation);
+		userLocation = targetLocation;
+		displayWorld();
 	}); 
-
 });
 
-function apiQuery(location)
+function loadUniverse(location)
 {
 	clear();
 
-	$.ajax({ type: "POST", url: "http://api.xxiivv.com/paradise/beholder", data: { values:"{\"owner\":\""+owner+"\",\"location\":\""+location+"\"}" }
+	$.ajax({ type: "POST", url: "http://api.xxiivv.com/paradise/beholder", data: { values:"{\"owner\":\""+universeId+"\",\"location\":\""+location+"\"}" }
 	}).done(function( content_raw ) {
-
 		var parsed = JSON.parse(content_raw);
-		var parsed_location = null;
-		var parsed_vessels = [];
-
-		i = 0;
-		while( i<parsed.length){
-			if(parsed[i][0] == location){
-				parsed_location = parsed[i];
-			}
-			else{
-				parsed_vessels.push(parsed[i]);
-			}
-			i++;
-		}
-
-		$("#location").html(parsed_location[1]);
-		$("#note").html(parsed_location[4]);
-
-		i = 0;
-		while( i<parsed_vessels.length){
-			$("#vessels").append("<vessel data='"+parsed_vessels[i][0]+"'>"+parsed_vessels[i][1]+"</vessel><br />");
-			i++;
-		}
+		universeArray = parsed;
+		saveWorld(universeArray);
 	});
+}
+
+function saveWorld(universeArray)
+{
+	console.log(universeArray);
+	displayWorld();
+}
+
+function displayWorld()
+{
+	clear();
+	
+	var parsed_location = null;
+	var parsed_visibles = [];
+
+	i = 0;
+	while( i<universeArray.length){
+		if( universeArray[i][0] == userLocation ){
+			parsed_location = universeArray[i];
+		}
+		else if( universeArray[i][3] == userLocation ){
+			parsed_visibles.push(universeArray[i]);
+		}
+		i++;
+	}
+
+	$("#location").html(parsed_location[1]);
+	$("#note").html(parsed_location[4]);
+
+	i = 0;
+	while( i < parsed_visibles.length){
+		$("#vessels").append("<vessel data='"+parsed_visibles[i][0]+"'>"+parsed_visibles[i][1]+"</vessel><br />");
+		i++;
+	}
 }
 
 function clear()
